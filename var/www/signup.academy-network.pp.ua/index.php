bb813aa95beb75ffcc22aa669c6913832117415c
samuel-edmund-morgan <samuel.edmund.morgan@gmail.com>
2023-06-22 12:18:48 +0300

    Changed vendored css files

--- var/www/signup.academy-network.pp.ua/index.php
+++ var/www/signup.academy-network.pp.ua/index.php
@@ -5,6 +5,7 @@
   <title>Академія СБУ: Реєстрація в системі</title>
   <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
   <link rel="stylesheet" href="assets/css/bootstrap.css">
+  <link rel="stylesheet" href="assets/css/academy.css">
   <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body onload="  ">
